b4c9940ce3dd4a530f89c2c89df226477cee13e1
Ngoc Dinh Khoa Nguyen <157516884+KhoaDinhNguyen@users.noreply.github.com>
2026-07-24 10:50:43 -0700

    Add: partition-labels - submission-2

---
+++ Data Structures & Algorithms/partition-labels/submission-2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    /**
+    The idea is that if the last index is the current index, push the length into result
+    **/
+    vector<int> partitionLabels(string s) {
+        int lastIndexes[26] = {};
+
+        for (int i = 0 ; i < s.size(); ++i){
+            lastIndexes[s[i] - 'a'] = i;
+        }
+
+        vector<int> res;
+        int idx = 0, start = 0, N = s.size();
+        
+
+        while (idx < N) {
+            int lastIndex = lastIndexes[s[idx] - 'a'];
+
+            while (idx < lastIndex) {
+                lastIndex = max(lastIndex, lastIndexes[s[idx] - 'a']);
+                
+                if (lastIndex == N) {
+                    res.push_back(N - start);
+                    return res;
+                }
+                idx++;
+            }
+            // if current index == lastIndex, it each letter has appears in one substring
+            res.push_back(idx - start + 1);
+            idx = idx + 1;
+            start = idx;
+        }
+
+        return res;
+    }
+};
