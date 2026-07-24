50598eb420273918d0441e9d5d5900b4e29101fb
Ngoc Dinh Khoa Nguyen <157516884+KhoaDinhNguyen@users.noreply.github.com>
2026-07-24 13:35:58 -0700

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int N = coins.size();
+
+        vector<int> dp(amount + 1, 100000);
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (coins[j] == i) {
+                    dp[i] = 1;
+                }
+                else if (i > coins[j]) {
+                    dp[i] = min(dp[i], dp[i - coins[j]] + dp[coins[j]]);
+                }
+            }
+        }
+
+        // Time complexity: O(n * t)
+        // Space complexity: O(t)
+        return dp[amount] == 100000? -1: dp[amount];
+    }
+};
