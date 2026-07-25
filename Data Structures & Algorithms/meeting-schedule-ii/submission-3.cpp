6d9ad6b887b4995a7568469a6933da16b7078ef9
Ngoc Dinh Khoa Nguyen <157516884+KhoaDinhNguyen@users.noreply.github.com>
2026-07-24 21:42:43 -0700

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    /**
+    Another idea is to use two array starts and ends
+    - Then we use two pointers s and e
+    - Whenever s < e we increase the number of rooms
+    **/
+    int minMeetingRooms(vector<Interval>& intervals) {
+        if (intervals.size() == 0) return 0;
+        
+        vector<int> start, end;
+
+        int N = intervals.size();
+        for (int i = 0; i < N; ++i) {
+            start.push_back(intervals[i].start);
+            end.push_back(intervals[i].end);
+        }
+
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+
+        int s = 1, e = 0, activeRoom = 1;
+
+        while (s < N) {
+            if (start[s] < end[e]) {
+                activeRoom++;       
+            }
+            else {
+                e++;
+            }
+            s++;
+        }
+
+        return activeRoom;
+    }
+};
