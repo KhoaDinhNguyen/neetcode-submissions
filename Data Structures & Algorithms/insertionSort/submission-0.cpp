1782cfb2d1f0b906c119c01021d2a668f2a44bc3
Ngoc Dinh Khoa Nguyen <157516884+KhoaDinhNguyen@users.noreply.github.com>
2026-07-25 19:36:54 -0700

    Add: insertionSort - submission-0

---
+++ Data Structures & Algorithms/insertionSort/submission-0.cpp
@@ -0,0 +1,38 @@
+// Definition for a Pair
+// class Pair {
+// public:
+//     int key;
+//     string value;
+//
+//     Pair(int key, string value) : key(key), value(value) {}
+// };
+class Solution {
+public:
+    bool comparePair(Pair& i, Pair& j) {
+        return i.key <= j.key;
+    }
+
+    vector<vector<Pair>> insertionSort(vector<Pair>& pairs) {
+        vector<vector<Pair>> res;
+        int N = pairs.size();
+
+        for (int i = 0; i < N; ++i) {
+            int j = i;
+            while (j > 0) {
+                // stop when pairs[j] > pairs[j - 1]
+                if (comparePair(pairs[j - 1], pairs[j])) break;
+
+                // Else swap pairs[j] and pairs[j - 1]
+                Pair temp = pairs[j - 1];
+                pairs[j - 1] = pairs[j];
+                pairs[j] = temp;
+                j--;
+
+            }
+
+            res.push_back(pairs);
+        }
+
+        return res;
+    }
+};
