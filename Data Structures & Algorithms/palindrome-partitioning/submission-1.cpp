7920e9e9924db59affd1ff77b9df5a9a91bfc577
Ngoc Dinh Khoa Nguyen <157516884+KhoaDinhNguyen@users.noreply.github.com>
2026-07-29 13:26:31 -0700

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+
+    bool checkPalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+
+        while (i < j) {
+            if (s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+    void backtrack(string& s, int idx, string prevWord, vector<string> res) {
+        int N = s.size();
+
+        if (idx == N) {
+            if (prevWord == "") ans.push_back(res);
+            return;
+        }
+
+        // Option 1: make partition and start new string        
+        string word = prevWord + s[idx];
+
+        // Check palindrome
+        if (checkPalindrome(word)) {
+            res.push_back(word);
+            backtrack(s, idx + 1, "", res);
+            res.pop_back();
+        }
+
+        // Option 2: Continue
+        
+        backtrack(s, idx + 1, word, res);
+    }
+    vector<vector<string>> partition(string s) {
+        vector<string> res;
+
+        backtrack(s, 0, "", res);
+    
+        return ans;
+    }
+};
