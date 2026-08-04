3cde35e0bd7b8c7bc9841eaddfc84b08d25d68c7
Ngoc Dinh Khoa Nguyen <157516884+KhoaDinhNguyen@users.noreply.github.com>
2026-08-03 17:57:07 -0700

    Add: permutations-ii - submission-1

---
+++ Data Structures & Algorithms/permutations-ii/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    set<vector<int>> st;
+
+    void backtrack(vector<int>& nums, vector<int>& res, vector<bool>& visited) {
+        if (res.size() == nums.size()) {
+            st.insert(res);
+            return;
+        }
+
+        int N = nums.size();
+
+        for (int i = 0; i < N; ++i) {
+            if (visited[i] || (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])) continue;
+
+            res.push_back(nums[i]);
+            visited[i] = true;
+            backtrack(nums, res, visited);
+
+            res.pop_back();
+            visited[i] = false;
+        }
+
+        
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<int> res;
+        vector<bool> visited(nums.size(), false);
+
+        backtrack(nums, res, visited);
+
+        vector<vector<int>> ans;
+
+        auto it = st.begin();
+
+        while (it != st.end()) {
+            ans.push_back(*it);
+            it++;
+        }
+
+        return ans;
+    }
+};
