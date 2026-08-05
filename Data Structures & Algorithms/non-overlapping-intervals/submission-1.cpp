4ef64ccd300c6a76db344584f46a161b1987a46a
Ngoc Dinh Khoa Nguyen <157516884+KhoaDinhNguyen@users.noreply.github.com>
2026-08-05 11:48:37 -0700

    Add: non-overlapping-intervals - submission-1

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    /**
+    The idea is that we only care about the end, and for greedy, we want to take the end that has minimum value
+    **/
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+
+        int prevEnd = intervals[0][1];
+        int ans = 0;
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i][0] < prevEnd) {
+                prevEnd = min(prevEnd, intervals[i][1]);
+                ans++;
+            }
+            else {
+                prevEnd = intervals[i][1];
+            }
+        }
+
+        return ans;
+    }
+};
