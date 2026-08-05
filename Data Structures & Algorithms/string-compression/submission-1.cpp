512bf073569328d51230f8fa87772faa3266a370
Ngoc Dinh Khoa Nguyen <157516884+KhoaDinhNguyen@users.noreply.github.com>
2026-08-05 10:15:47 -0700

    Add: string-compression - submission-1

---
+++ Data Structures & Algorithms/string-compression/submission-1.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        if (chars.size() == 1) return 1;
+
+        char currentChar = chars[0];
+        int count = 1;
+        int ans = 0;
+        int j = 0;
+        
+        for (int i = 1; i < chars.size(); ++i) {
+            if (chars[i] == currentChar) {
+                count++;
+            }
+            else {
+                chars[j++] = currentChar;
+                if (count == 1) {
+                    ans += 1;
+                }
+                else {
+                    string count_string = to_string(count);
+                    for (int k = 0; k < count_string.size(); ++k) {
+                        chars[j++] = count_string[k];
+                    }
+                    ans += (1 + count_string.size());
+                }
+                
+                count = 1;
+                currentChar = chars[i];
+            }
+        }
+
+        chars[j++] = currentChar;
+        if (count == 1) {
+            ans += 1;
+        }
+        else {
+            string count_string = to_string(count);
+            for (int k = 0; k < count_string.size(); ++k) {
+                chars[j++] = count_string[k];
+            }
+            ans += (1 + count_string.size());
+        }
+
+        return ans;
+
+    }
+
+    // Time: O(N)
+    // Space: O(1)
+};
