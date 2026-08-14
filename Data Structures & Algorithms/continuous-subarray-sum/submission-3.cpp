533fd7ad203dd57a8f928389f723e502b58695e6
Ngoc Dinh Khoa Nguyen <157516884+KhoaDinhNguyen@users.noreply.github.com>
2026-08-14 12:44:58 -0700

    Add: continuous-subarray-sum - submission-3

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int N = nums.size();
+
+        unordered_map<int, int> remainder;
+        remainder[0] = -1;
+
+        int total = 0;
+
+        for (int i = 0; i < N; ++i) {
+            total += nums[i];
+            int r = total % k;
+
+            if (remainder.count(r) == 0) {
+                remainder[r] = i;
+            }
+            else if (i - remainder[r] > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
